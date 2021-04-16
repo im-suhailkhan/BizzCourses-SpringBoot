63a682fd7901cf5e0ed9aaf696c6d1d4c457ca3b
suhailkhanqbox <suhail.khan.ftg@gmail.com>
2021-04-17 00:29:27 +0530

    intital boilercode

---
+++ BizzCourses/src/main/java/com/Courses/BizzCourses/BizzCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.Courses.BizzCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizzCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizzCoursesApplication.class, args);
+	}
+
+}
---
+++ BizzCourses/src/test/java/com/Courses/BizzCourses/BizzCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Courses.BizzCourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizzCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
